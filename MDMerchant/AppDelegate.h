d423fae129cd5e638364d17019a67d1cd6ef2452
geekxing <842775263@qq.com>
2017-08-06 20:01:23 +0800

    Initial Commit

---
+++ MDMerchant/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MDMerchant
+//
+//  Created by apple on 2017/8/6.
+//  Copyright © 2017年 Wuxi Kaishang Internet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
